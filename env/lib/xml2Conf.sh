37feacb9dc5fea422c4ce55cbfbe851138693284
Caleb KM <calebkm@gmail.com>
2020-05-07 18:21:01 -0700

    Update .gitignore.

--- env/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/Users/calebkm/Projects/lane-finding/env/lib"
-XML2_LIBS="-lxml2 -L/Users/calebkm/Projects/lane-finding/env/lib -lz -L/Users/calebkm/Projects/lane-finding/env/lib -llzma -lpthread -L/Users/calebkm/Projects/lane-finding/env/lib -liconv  -lm "
-XML2_INCLUDEDIR="-I/Users/calebkm/Projects/lane-finding/env/include/libxml2 -I/Users/calebkm/Projects/lane-finding/env/include"
-MODULE_VERSION="xml2-2.9.9"
-
